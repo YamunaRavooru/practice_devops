21c7a59f4fd0b02f633a8c703778e46fb722f67d
yamuna <malini.mahathi@gmail.com>
2025-01-19 19:24:31 +0530

    practice

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 Log_files="$Source_dirc/$File_name-$TIMESTAMP.log"
 D_Dir=$1
 S_Dir=$2
-Days=${$3:-14}
+Days=${3:-14}
 if [ $# -lt 2 ]
 then
   echo "Usage: sh backup <S_Dir> <D_Dir> days <optional>"
