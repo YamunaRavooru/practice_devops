52cca6ff9dc8b33183935150700e730cb9fa184b
yamuna <malini.mahathi@gmail.com>
2025-01-19 21:22:47 +0530

    practice

--- backup.sh
+++ backup.sh
@@ -33,7 +33,7 @@ then
      echo "Deleted files :$files"
      Zip_file="$D_Dir/app-log-$TIMESTAMP.zip"  &>>Log_files
     find $S_Dir -name  "*.log" -mtime +$Days | zip -@  "$Zip_file" &>>Log_files
-  if [ -f  "$Zip_file" ]
+  if [ -n  "$Zip_file" ]
   then
       echo -e "Zip file is $Y sucessfully $N created older than $Days..."
       while  read -r  $name
