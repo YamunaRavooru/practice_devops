0c37173f0dfd2e73b4b71f1bd110d16c4240ccc9
yamuna <malini.mahathi@gmail.com>
2025-01-19 18:13:35 +0530

    practice

--- delete-log-file.sh
+++ delete-log-file.sh
@@ -4,7 +4,7 @@ Source_dirc="/home/ec2-user/app-log"
 Dest_dirc="/var/log/Shell_logs"
 File_Name=$(echo  $0 | cut -d '.' -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-Log_files= "$Dest_dirc/$File_Name-$TIMESTAMP.log"
+Log_files="$Dest_dirc/$File_Name-$TIMESTAMP.log"
 
 user=$(id -u)
 if [ $user -ne 0 ]
