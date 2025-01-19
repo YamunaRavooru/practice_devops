ea8bd07a3465720114f048cd4c169f6fb0412ab7
yamuna <malini.mahathi@gmail.com>
2025-01-19 17:52:04 +0530

    practice

---
+++ delete-log-file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+Source_dirc="/home/ec2-user/app-log"
+Dest_dirc="/var/log/Shell_logs"
+File_Name=$( $0 | cut -d '.' -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+Delete_files= "$Dest_dirc/$File_Name-$TIMESTAMP.log"
+
+user=$(id -u)
+if [ $user -ne 0 ]
+then
+  echo Root user only acess this script:  
+  exit 1
+fi
+  
